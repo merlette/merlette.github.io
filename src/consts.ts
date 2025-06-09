85a3c64cf7e326aa3391dc4e5b3adfc61156018e
Merlette <merlette@aliyun.com>
2025-06-09 16:06:04 +0800

    25/6/9

--- src/consts.ts
+++ src/consts.ts
@@ -2,49 +2,60 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_TAB = "Merlette's Blog";
-export const SITE_TITLE = "Merlette's Blog";
-export const SITE_DESCRIPTION = "A blog hidden in the corners of the internet.";
+export const SITE_LANG = "en";
+export const SITE_TAB = "Merlette";
+export const SITE_TITLE = "Merlette";
+export const SITE_DESCRIPTION = "A homepage hidden in the corners of the internet.";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
 export const USER_NAME = "Merlette";
-export const USER_SITE = "https://merlette.github.io"
+export const USER_SITE = "https://merlette.github.io"; // At the same time, this is also the site retrieved by the i18n configuration.
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
 export const SERVER_URL = "https://merlette.github.io";
 
+// Theme settings
+export const DAISYUI_THEME = {
+  light: "winter",
+  dark: "dracula",
+};
+export const CODE_THEME = {
+  light: "github-light",
+  dark: "github-dark",
+};
+
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "Home", href: "/", svg: "home", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "about", target: "_self" }, // About page
+  { id: "home", text: "Home", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
+  { id: "about", text: "About", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
   {
     id: "blog",
     text: "Blogs",
     href: "/blog",
-    svg: "blog",
+    svg: "material-symbols:book-2-outline-rounded",
     target: "_self",
     subItems: [
       {
         id: "all",
         text: "All blogs",
         href: "/blog",
-        svg: "post",
+        svg: "material-symbols:ink-pen-outline-rounded",
         target: "_self",
       }, // All blog
       {
         id: "tech",
         text: "Tech blogs",
         href: "/blog/categories/tech",
-        svg: "cube",
+        svg: "material-symbols:deployed-code-outline",
         target: "_self",
       }, // Technology category
       {
         id: "life",
         text: "Life blogs",
         href: "/blog/categories/life",
-        svg: "heart",
+        svg: "material-symbols:earthquake-rounded",
         target: "_self",
       }, // Life category
     ],
@@ -53,49 +64,59 @@ export const menuItems = [
     id: "project",
     text: "Project",
     href: "/project",
-    svg: "project",
+    svg: "material-symbols:code-blocks-outline",
     target: "_self",
   }, // Projects page
+  /*
   {
     id: "friend",
     text: "Friend",
     href: "/friend",
-    svg: "friend",
+    svg: "material-symbols:supervisor-account-outline-rounded",
     target: "_self",
   }, // Friends page
+  */
   {
     id: "contact",
     text: "Contact",
     href: "mailto:merlette@aliyun.com", // Contact email
     target: "_blank", // Open in a new tab
-    svg: "contact",
+    svg: "material-symbols:attach-email-outline-rounded",
   },
 ];
 
 // Social media and contact icons
 export const socialIcons = [
-  // {
-  //   href: "https://afdian.net/a/saroprock",
-  //   ariaLabel: "Support my work",
-  //   title: "Support my work",
-  //   svg: "support",
-  // },
   {
-    href: "https://github.com/Merlette",
+    href: "https://github.com/merlette",
     ariaLabel: "Github",
     title: "Github",
-    svg: "github",
+    svg: "mingcute:github-line",
   },
   {
-    href: "https://space.bilibili.com/519830795",
+    href: "https://space.bilibili.com/438392347",
     ariaLabel: "BiliBili",
     title: "BiliBili",
-    svg: "bilibili",
+    svg: "mingcute:bilibili-line",
   },
+  {
+    href: "https://www.youtube.com/@merlet_te",
+    ariaLabel: "YouTube",
+    title: "YouTube",
+    svg: "mingcute:youtube-line",
+  },
+  {
+    href: "https://x.com/merlet_te",
+    ariaLabel: "X",
+    title: "X",
+    svg: "mingcute:social-x-line",
+  },
+  /*
   {
     href: "/rss.xml",
     ariaLabel: "RSS Feed",
     title: "RSS Feed",
-    svg: "rss",
+    svg: "ri:rss-line",
   },
+  */
 ];
